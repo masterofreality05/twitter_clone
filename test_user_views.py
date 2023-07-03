8868d22d3fa06ed26864265e2f6f8e44421189ca
SEAN WHITTY <SEANWHITTY92@gmail.com>
2023-07-03 20:51:47 +0200

    logged in and logged out view functions tested successfull

---
+++ test_user_views.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from app import app, do_logout
+
+
+class TestDbManipulation(TestCase):
+    """unit tests to measure our sql interaction with crud"""
+
+    def setUp(self):
+        """clean up any existing pets by clearing rows in our table"""
+
+ 
+
+    def tearDown(self):
+        """clean up any fouled transaction with rollback"""
+
+
+    
+
+    def test_homepage_logged_out(self):
+        """test the homepage view with no user logged in"""
+        with app.test_client() as client:
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("Sign up now to get your own personalized timeline!", html)
+    
+    def test_homepage_logged_in(self):
+       """test the homepage view with no user logged in"""
+       app.config['WTF_CSRF_ENABLED'] = False
+       with app.test_client() as client:
+            res = client.post("/login", data={'username':'damo','password':'ghbghb'},
+            follow_redirects=True)
+            html = res.get_data(as_text=True)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("Hello", html)
+            do_logout()
+       
+
+
